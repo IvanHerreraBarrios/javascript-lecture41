2de95219f7eafe99705c4a65b0f1c0c11687beba
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-11-18 13:57:08 -0600

    leetcode-lecture41

---
+++ app.js
@@ -0,0 +1,10 @@
+var hasAllCodes = function(s, k) {
+    let set = new Set();
+
+    for (let i = k; i <= s.length; i++)
+    {
+        set.add(s.slice(i - k, i));
+    }
+    
+    return set.size === Math.pow(2, k);
+};
